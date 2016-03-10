359f4c89f58c098f6417e566ff8eafa29d14370f
alynsther <ayang@bowdoin.edu>
2016-03-10 14:29:40 -0500

    m

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -13,26 +13,98 @@ class CalculatorBrain
     /***************************************************************************/
     /* global variables */
     private var opStack = [Op]()
-    private var knownOps = [String:Op]()
-    
+    private var knownOps = [String: Op]()
+    private var variableValues = [String: Double]()
+
     /***************************************************************************/
      /* different types of operand and operations */
-    private enum Op {
+    private enum Op:CustomStringConvertible {
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+        case Variable(String)
+        
+        //read only property returns a strin
+        var description: String {
+            get {
+                switch self {
+                    case .Operand(let operand):
+                        return "\(operand)"
+                    case .UnaryOperation(let symbol, _):
+                        return symbol
+                    case .BinaryOperation(let symbol, _):
+                        return symbol
+                    case .Variable(let symbol):
+                        return "\(symbol)"
+                }
+            }
+        }
+
     }
     
+    /***************************************************************************
+     Function:  program
+     Inputs:    any object
+     Returns:
+     Description:
+     ***************************************************************************/
+    var program: AnyObject { // guaranteed to be a PropertyList
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    }
+                    else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+            }
+        }
+    }
+
+    /***************************************************************************
+     Function:  description
+     Inputs:
+     Returns:
+     Description: read-only that describes the contents of the brain as a String
+     ***************************************************************************/
+    var description: String {
+        get {
+            var (result, ops) = ("", opStack)
+            repeat {
+                var value: String?
+                (value, ops) = description(ops)
+                if (result == ""){
+                    result = value!
+                }
+                else {
+                    result = "\(value!), \(result)"
+                }
+            } while ops.count > 0
+            return result
+        }
+    }
+
     /***************************************************************************/
      /* initialize the operations */
     init() {
-        knownOps["×"] = Op.BinaryOperation("×", *)
-        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
-        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
-        knownOps["+"] = Op.BinaryOperation("+", +)
-        knownOps["√"] = Op.UnaryOperation("√", sqrt)
-        knownOps["sin"] = Op.UnaryOperation("sin", sin)
-        knownOps["cos"] = Op.UnaryOperation("cos", cos)
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("×", * ))
+        learnOp(Op.BinaryOperation("+", + ))
+        learnOp(Op.BinaryOperation("÷", { $1 / $0 } ))
+        learnOp(Op.BinaryOperation("−", { $1 - $0 } ))
+        learnOp(Op.UnaryOperation("√", sqrt ))
+        learnOp(Op.UnaryOperation("sin", sin ))
+        learnOp(Op.UnaryOperation("cos", cos ))
     }
 
     /***************************************************************************
@@ -83,23 +155,27 @@ class CalculatorBrain
             let op = remainingOps.removeLast()
         
             switch op {
-            case .Operand(let operand):
-                return (operand, remainingOps)
-            case .UnaryOperation(_, let operation):
-                let operandEvaluation = evaluate(remainingOps)
-                if let operand = operandEvaluation.result {
-                    return (operation(operand), operandEvaluation.remainingOps)
-                }
-            case .BinaryOperation(_, let operation):
-                let operandEvaluation = evaluate(remainingOps)
-                if let operand = operandEvaluation.result {
-                    let operandEvaluation2 = evaluate(operandEvaluation.remainingOps)
-                    if let operand2 = operandEvaluation2.result {
-                        return (operation(operand, operand2), operandEvaluation2.remainingOps)
+                case .Operand(let operand):
+                    return (operand, remainingOps)
+                case .UnaryOperation(_, let operation):
+                    let operandEvaluation = evaluate(remainingOps)
+                    if let operand = operandEvaluation.result {
+                        return (operation(operand), operandEvaluation.remainingOps)
                     }
-                }
+                case .BinaryOperation(_, let operation):
+                    let operandEvaluation = evaluate(remainingOps)
+                    if let operand = operandEvaluation.result {
+                        let operandEvaluation2 = evaluate(operandEvaluation.remainingOps)
+                        if let operand2 = operandEvaluation2.result {
+                            return (operation(operand, operand2), operandEvaluation2.remainingOps)
+                        }
+                    }
+                case .Variable(let symbol):
+                    if variableValues[symbol] == nil {
+                        return (nil, ops)
+                    }
+                    return (variableValues[symbol], remainingOps)
             }
-        
         }
         
         return (nil, ops)
@@ -113,10 +189,104 @@ class CalculatorBrain
      ***************************************************************************/
     func clearBrain() {
         opStack.removeAll()
+        variableValues = [String: Double]()
     }
     
     
+    /***************************************************************************
+     Function:  pushOperand (assignment 2)
+     Inputs:    String
+     Returns:   Double
+     Description: pushes a variable onto your brain's stack
+        should return the result of evaluate after having pushed the variable
+     ***************************************************************************/
+    func pushOperand(symbol: String) -> Double? {
+        opStack.append(Op.Variable(symbol))
+        return evaluate()
+    }
+    
+    /***************************************************************************
+    Function:   setVariable
+    Inputs:     string and double
+    Returns:    double
+    Description: lets users of brain set the value for any variable they wish
+    ***************************************************************************/
+    func setVariable(symbol: String, value: Double) -> Double? {
+        variableValues[symbol] = value
+        return evaluate()
+    }
+    
+    /***************************************************************************
+     Function:  description
+     Inputs:    array of Op
+     Returns:   tuple
+     Description: evaluates the stack recursively
+     ***************************************************************************/
+    private func description(ops: [Op]) -> (result: String?,  remainingOps: [Op]){
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                if operand == M_PI{
+                    return ("∏", remainingOps)
+                }
+                else {
+                    return ("\(operand)", remainingOps)
+                }
+            case .UnaryOperation(let symbol, _):
+                let operandEvaluation = description(remainingOps)
+                if let operand = operandEvaluation.result {
+                    if operand == String(M_PI){
+                        return ("\(symbol)(∏)", operandEvaluation.remainingOps)
+                    }
+                    else {
+                        return ("\(symbol)(\(operand))", operandEvaluation.remainingOps)
+                    }
+                }
+            case .BinaryOperation(let symbol, _):
+                let operandEvaluation = description(remainingOps)
+                if var operand = operandEvaluation.result {
+                    if operand == String(M_PI){
+                        if remainingOps.count - operandEvaluation.remainingOps.count > 2 {
+                            operand = "(∏)"
+                        }
+                        let operandEvaluation2 = description(operandEvaluation.remainingOps)
+                        if let operand2 = operandEvaluation2.result {
+                            if operand2 == String(M_PI) {
+                                return ("∏ \(symbol) ∏", operandEvaluation2.remainingOps)
+                            }
+                            else {
+                                return ("\(operand2) \(symbol) ∏", operandEvaluation2.remainingOps)
+                            }
+                        }
+                    }
+                    else {
+                        if remainingOps.count - operandEvaluation.remainingOps.count > 2 {
+                            operand = "(\(operand))"
+                        }
+                        let operandEvaluation2 = description(operandEvaluation.remainingOps)
+                        if let operand2 = operandEvaluation2.result {
+                            if operand2 == String(M_PI) {
+                                return ("∏ \(symbol) \(operand)", operandEvaluation2.remainingOps)
+                            }
+                            else {
+                                return ("\(operand2) \(symbol) \(operand)", operandEvaluation2.remainingOps)
+                            }
+                        }
+                    }
+                }
+            case .Variable(let symbol):
+                return (symbol, remainingOps)
+            }
+        }
+        
+        return ("?", ops)
+    }
 
+    
+    
 } // end class
 
 /****** END OF FILE ********************************************************/
--- YangCalculator/ViewController.swift
+++ YangCalculator/ViewController.swift
@@ -21,8 +21,10 @@ class ViewController: UIViewController {
     var userIsInTheMiddleOfTypingANumber: Bool = false
     var decimalWasPressed: Bool = false
     var piWasPressed = 0
-    var brain = CalculatorBrain()
     let x = M_PI // pi functiong
+    var started: Bool = false
+    var enterFirst: Bool = false
+    var brain = CalculatorBrain()
     
     @IBOutlet weak var display: UILabel!
 
@@ -36,6 +38,7 @@ class ViewController: UIViewController {
      ***************************************************************************/
     @IBAction func digitPressed(sender: UIButton) {
         let digit = sender.currentTitle!
+        started = true
         
         if userIsInTheMiddleOfTypingANumber {
             display.text = display.text! + digit
@@ -52,17 +55,20 @@ class ViewController: UIViewController {
      Description: updates the operation to the history and calculates the answer
      ***************************************************************************/
     @IBAction func operate(sender: UIButton) {
+        started = true
+        
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         
         if let operation = sender.currentTitle {
-            updateHistory("\(operation)")
-            
             if let result = brain.performOperation(operation) {
                 displayValue = result
-            } else {
-                displayValue = 0
+                history.text = history.text! + "="
+            }
+            else {
+                displayValue = nil
+                history.text = history.text!
             }
         }
     }
@@ -77,19 +83,23 @@ class ViewController: UIViewController {
         userIsInTheMiddleOfTypingANumber = false
         decimalWasPressed = false
         
-        if let result = brain.pushOperand(displayValue) {
-            displayValue = result
-        } else {
-            displayValue = 0
-        }
-        
         if piWasPressed > 0 {
-            updateHistory("∏")
+            brain.pushOperand(x)
             piWasPressed--
-        } else {
-            updateHistory("\(displayValue)")
         }
-        
+        else {
+            if let varValue = displayValue {
+                if let result = brain.pushOperand(varValue) {
+                    displayValue = result
+                } else {
+                    displayValue = 0
+                }
+            }
+            else {
+                displayValue = nil
+            }
+        }
+        enterFirst = true
     }
     
     /***************************************************************************
@@ -98,9 +108,14 @@ class ViewController: UIViewController {
      Returns:   none
      Description: updates the history and display value to pi
      ***************************************************************************/
-    @IBAction func piPressed() {
-        enter()
+    @IBAction func piPressed(sender: UIButton) {
+        let piSign = sender.currentTitle!
+        if started{
+            enter()
+        }
+        
         piWasPressed = 1
+        display.text = piSign
         displayValue = x
         enter()
         piWasPressed = 0
@@ -114,6 +129,8 @@ class ViewController: UIViewController {
      ***************************************************************************/
     @IBAction func decimalPressed() {
         userIsInTheMiddleOfTypingANumber = true
+        started = true
+        
         if decimalWasPressed == false {
             display.text = display.text! + "."
             decimalWasPressed = true
@@ -129,39 +146,88 @@ class ViewController: UIViewController {
     @IBAction func clear() {
         userIsInTheMiddleOfTypingANumber = false
         decimalWasPressed = false
+        started = false
+        enterFirst = false
+        
         piWasPressed = 0
-        displayValue = 0
+        display.text = ""
         history.text = ""
         brain.clearBrain()
     }
     
+ 
+    @IBAction func getM(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+             // need to test this with a decimal
+        }
+        
+        if let result = brain.pushOperand("M") {
+            displayValue = result
+        }
+        else {
+            displayValue = nil
+        }
+        
+    }
+
+    
+    @IBAction func setM(sender: UIButton) {
+        userIsInTheMiddleOfTypingANumber = false
+
+        if let result = brain.setVariable("M", value: NSNumberFormatter().numberFromString(display.text!)!.doubleValue) {
+            displayValue = result
+        }
+        else {
+            displayValue = nil
+        }
+
+    }
     
     /***************************************************************************/
      /* utility functions */
     
-    /***************************************************************************
-     Function:  updateHistory
-     Inputs:    String
-     Returns:   none
-     Description: displays the history
-     ***************************************************************************/
-    func updateHistory(value: String) {
-        history.text = history.text! + " " + value
-    }
-
     /***************************************************************************
      Function:  displayValue
      Inputs:    Double
      Returns:   none
      Description: displays the value
      ***************************************************************************/
-    var displayValue: Double {
+    var displayValue: Double? {
         get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            if let varValue = NSNumberFormatter().numberFromString(display.text!)?.doubleValue {
+                return varValue
+            }
+            else {
+                return nil
+            }
         }
+        //possible difference here?
         set {
-            display.text = "\(newValue)"
+            if let varValue = newValue {
+                if varValue != M_PI{
+                    
+                    display.text = "\(varValue)"
+                }
+
+            }
+            else {
+                display.text = ""
+            }
+            
+            if(newValue != M_PI) {
+                history.text = brain.description ?? " "
+            }
+            else {
+                if !enterFirst {
+                    history.text = "∏"
+                }
+                else {
+                    history.text = history.text! + ",∏"
+                }
+            }
             userIsInTheMiddleOfTypingANumber = false
+            
         }
     }
 
