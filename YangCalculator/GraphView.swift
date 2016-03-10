2d75616f0d263d51eb0baac236ab898ceb9a4a40
alynsther <ayang@bowdoin.edu>
2016-03-10 15:55:10 -0500

    m

---
+++ YangCalculator/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  YangCalculator
+//
+//  Created by Adela  Yang on 3/10/16.
+//  Copyright © 2016 edu.bowdoin.cs3400.ayang. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
